731efdfb7181ebda9219285583ac1fe7884ea45f
Abhay Upadhyay <abhay281103@gmail.com>
2023-05-17 23:55:39 +0530

    update create.jsx

--- Client/src/Pages/Create.jsx
+++ Client/src/Pages/Create.jsx
@@ -1,16 +1,19 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import Select from 'react-select'
 import './create.css'
 
 const SelectedOption =(props)=> {
   if(props.option==='Age restriction'){
   return(
-    <input placeholder='min age' type='number'/>
+    <>
+    <label htmlFor="" style={{marginTop:'10px'}}>Age range</label>
+    <input style={{margin: '10px 0 10px 0'}} placeholder='min age' type='number'/>
+    </>
   )
   }
   if(props.option==='Twitter following restriction'){
     return(
-      <p>
+      <p style={{margin: '10px 0 10px 0'}}>
         Feature not available yet !
       </p>
     )
@@ -24,16 +27,14 @@ const Create = () => {
     { value: 'Age restriction', label: 'Age' },
     { value: 'Twitter following restriction', label: 'Social'},
   ]
-
-  useEffect(()=>{console.log(selected);})
   return (
     <div className='create-dao'>
       Create DAO
       <label htmlFor="daoCapacity">DaoCapacity</label>
-      <input type="number" id='daoCapacity'/>
+      <input type="number" id='daoCapacity' min={1}/>
       <label htmlFor="DAOend">DAO end date</label>
       <input type="date" name="DAOend" id="DAOend" />
-      <label htmlFor="">ZKP options</label>
+      <label htmlFor="zkpOtions">ZKP options</label>
       <Select className="basic-single"
         classNamePrefix="select"
         name="zkpOtions"
