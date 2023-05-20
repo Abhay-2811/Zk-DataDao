2ba6e5ba155dc5ed2307a2e7ae2fa58f804c86b3
Abhay Upadhyay <abhay281103@gmail.com>
2023-05-20 16:20:28 +0530

    discord auth addition

--- Client/src/Pages/DAOs.jsx
+++ Client/src/Pages/DAOs.jsx
@@ -1,19 +1,44 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { daos } from '../Constants/fakeDAOs'
-import { useSearchParams } from 'react-router-dom'
+import { useSearchParams, useLocation } from 'react-router-dom'
+import axios from 'axios'
 
-
-const DAOs = () => {
+const DAOs = props => {
+  let names = new Array();
   const [searchParams] = useSearchParams()
-  const Discordparam = searchParams.get('code');
-  if(Discordparam){
-    return(
-      <div>Param Exists</div>
-    )
+  const location = useLocation()
+  const Discordparam = location.hash?.split('&')[1]?.split('=')[1]
+  const Daoparam = searchParams.get('dao');
+  const get = async () => {
+    let config = {
+      method: 'get',
+      maxBodyLength: Infinity,
+      url: 'https://discord.com/api/users/@me/guilds',
+      headers: {
+        Authorization: `Bearer ${Discordparam}`,
+      }
+    }
+
+    axios
+      .request(config)
+      .then((response) => {
+        (response.data.map((data,index)=>{
+          names.push(data.name);
+        }))
+        return
+      })
+      .catch(error => {
+        console.log(error)
+      })
   }
-  else{
-    return(
-      <div>Param dne</div>
+  if (Discordparam && Daoparam) {
+    get();
+    return <div>Param Exists</div>
+  } else {
+    return (
+      <a href='https://discord.com/api/oauth2/authorize?client_id=1109245990302662787&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fdaos%3Fdao%3Dabc&response_type=token&scope=guilds'>
+        Press me
+      </a>
     )
   }
 }
